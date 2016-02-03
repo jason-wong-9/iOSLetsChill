3ae36a361211a9481abefb05f0f8eb9d7c6781ac
Jason Wong <jason_19960903@hotmail.com>
2016-02-03 09:58:01 -0800

    Install Firebase

--- Podfile
+++ Podfile
@@ -3,4 +3,5 @@ use_frameworks!
 
 target 'iOS Lets Chill' do
   pod 'GoogleMaps'
+  pod 'Firebase', '>= 2.5.0'
 end
---
+++ iOS Lets Chill/PhoneVerficationViewController.swift
@@ -0,0 +1,89 @@
+//
+//  PhoneVerficationViewController.swift
+//  iOS Lets Chill
+//
+//  Created by Jason Wong on 2016-02-03.
+//  Copyright © 2016 Jason Wong. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class PhoneVerficationViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var phoneTextField: UITextField!
+    
+    @IBAction func confirmAction(sender: AnyObject) {
+        
+        
+    }
+    
+    //let ref = Firebase(url: "https://letschill.firebaseio.com")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        phoneTextField.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool
+    {
+        if (textField == phoneTextField)
+        {
+            let newString = (textField.text! as NSString).stringByReplacingCharactersInRange(range, withString: string)
+            let components = newString.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet)
+            
+            let decimalString = components.joinWithSeparator("") as NSString
+            let length = decimalString.length
+            let hasLeadingOne = length > 0 && decimalString.characterAtIndex(0) == (1 as unichar)
+            
+            if length == 0 || (length > 10 && !hasLeadingOne) || length > 11
+            {
+                let newLength = (textField.text! as NSString).length + (string as NSString).length - range.length as Int
+                
+                return (newLength > 10) ? false : true
+            }
+            var index = 0 as Int
+            let formattedString = NSMutableString()
+            
+            if hasLeadingOne
+            {
+                formattedString.appendString("1 ")
+                index += 1
+            }
+            if (length - index) > 3
+            {
+                let areaCode = decimalString.substringWithRange(NSMakeRange(index, 3))
+                formattedString.appendFormat("(%@) ", areaCode)
+                index += 3
+            }
+            if length - index > 3
+            {
+                let prefix = decimalString.substringWithRange(NSMakeRange(index, 3))
+                formattedString.appendFormat("%@-", prefix)
+                index += 3
+            }
+            
+            let remainder = decimalString.substringFromIndex(index)
+            formattedString.appendString(remainder)
+            textField.text = formattedString as String
+            return false
+        }
+        else
+        {
+            return true
+        }
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?){
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
